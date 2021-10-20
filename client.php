7e5e539a16058e97870cbe20eec0831f09e052a0
v_jianghaiguang <v_jianghaiguang@baidu.com>
2021-10-20 15:29:22 +0800

    增加RPC,php案例

---
+++ client.php
@@ -0,0 +1,16 @@
+<?php
+
+$client = stream_socket_client("tcp://127.0.0.1:8887", $errno, $errstr);
+if(!$client) {
+	echo "异常代码：".$errno.",异常信息：".$errstr;exit;
+}
+$data['class'] = 'user';
+$data['method'] = 'get_name';
+// $data['param'] = 12;
+$_data = json_encode($data);
+fwrite($client, $_data);
+
+$server_data = fread($client, 2048);
+$result = json_decode($server_data);
+fclose($client);
+print_r($result);
---
+++ user.php
@@ -0,0 +1,7 @@
+<?php
+//用户服务类
+class user {
+	public function get_name(){
+		return 'ysm';
+	}
+}
